462afc1e7c83e9cba2ddc6df5dce1e07e4472a14
Olexander Yermakov <mannavard1611@gmail.com>
2015-03-08 01:53:56 +0200

    added names

--- lib/xarozed_name_generator.rb
+++ lib/xarozed_name_generator.rb
@@ -1,6 +1,15 @@
 require "xarozed_name_generator/version"
 require "xarozed_name_generator/markov_dict"
 
+
 module XarozedNameGenerator
-    print XarozedMarkovDict
+    class NameGenerator
+        def initialize(chainlen=2, max_len=9)
+            if chainlen > 1 and chainlen < 10
+                
+            else
+                raise ArgumentError, "Wrong chain length: #{chainlen}. Must be > 1 and < 10."
+            end
+        end
+    end
 end
--- lib/xarozed_name_generator/markov_dict.rb
+++ lib/xarozed_name_generator/markov_dict.rb
@@ -1,15 +1,14 @@
 module XarozedNameGenerator
     class MarkovDict
-        @makrov_dict = {}
-
         def initialize
+            @markov_dict = Hash.new
             @markov_dict.default = []
         end
-
+    
         def add_key(prefix, suffix)
             @markov_dict[prefix.to_sym] << suffix
         end
-
+    
         def get_suffix(prefix)
             @markov_dict[prefix].sample
         end
