f98ed20db2c18179259647c2690ad568a400015b
Olexander Yermakov <mannavard1611@gmail.com>
2015-03-08 03:26:51 +0200

    final version

--- lib/xarozed_name_generator.rb
+++ lib/xarozed_name_generator.rb
@@ -6,10 +6,40 @@ module XarozedNameGenerator
     class NameGenerator
         def initialize(chainlen=2, max_len=9)
             if chainlen > 1 and chainlen < 10
-                
+                path = File.dirname(__FILE__) + "/names/male_and_neutral"
+                name_list = File.readlines(path).join.split(" ")
+                @chainlen = chainlen
+                @max_len = max_len
+                @markov_dict = MarkovDict.new
+
+                name_list.collect do |name|
+                    s = ' ' * chainlen + name
+                    (0..name.length).each do |i|
+                        @markov_dict.add_key(s[i...i+chainlen], s[i+chainlen])
+                    end
+                    @markov_dict.add_key(s[name.length...name.length+chainlen], '\n')
+                end
             else
                 raise ArgumentError, "Wrong chain length: #{chainlen}. Must be > 1 and < 10."
             end
+        end
+
+        def name
+            prefix = ' ' * @chainlen
+            name = ''
+            suffix = ''
+
+            while true
+                suffix = @markov_dict.get_suffix(prefix)
+                if (suffix == '\n') or (name.length > @max_len)
+                    break
+                else
+                    name += suffix
+                    prefix = prefix[0...@chainlen-1] + suffix
+                end
+            end
+
+            return name.capitalize
         end
     end
 end
--- lib/xarozed_name_generator/markov_dict.rb
+++ lib/xarozed_name_generator/markov_dict.rb
@@ -2,15 +2,19 @@ module XarozedNameGenerator
     class MarkovDict
         def initialize
             @markov_dict = Hash.new
-            @markov_dict.default = []
+            @markov_dict.default = Array.new
         end
     
         def add_key(prefix, suffix)
-            @markov_dict[prefix.to_sym] << suffix
+            if not @markov_dict.has_key?(prefix)
+                @markov_dict[prefix] = [suffix]
+            else
+                @markov_dict[prefix].push(suffix)
+            end
         end
     
         def get_suffix(prefix)
-            @markov_dict[prefix].sample
+            return @markov_dict[prefix].sample
         end
     end
 end
